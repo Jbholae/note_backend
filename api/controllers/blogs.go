bd21b03578c5a6049b235dd0955d1e5e2106eaaf
Saurav Maharjan <saurav.maharjan7@gmail.com>
2023-07-12 17:39:24 +0545

    feat : created func

--- api/controllers/blogs.go
+++ api/controllers/blogs.go
@@ -7,6 +7,7 @@ import (
 	"boilerplate-api/models"
 	"boilerplate-api/responses"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -40,4 +41,73 @@ func (cc BlogsController) CreateBlog(c *gin.Context) {
 		return
 	}
 	responses.SuccessJSON(c, http.StatusOK, "Blog Created Successfully")
+}
+
+func (cc BlogsController) GetAllBlogs(c *gin.Context) {
+	cursor := c.Param("cursor")
+	blog, err := cc.blogServices.GetAllBlogs(cursor)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error [GetAllBlog] [dB GetAllBlog] : ", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to Get All Blogs")
+		responses.HandleError(c, err)
+		return
+	}
+	responses.JSON(c, http.StatusOK, blog)
+}
+
+func (cc BlogsController) GetOneBlog(c *gin.Context) {
+	blogId, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	blog, err := cc.blogServices.GetOneBlog(blogId)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error finding Blog : ", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to get Blog")
+		responses.HandleError(c, err)
+		return
+	}
+
+	responses.JSON(c, http.StatusOK, blog)
+}
+
+func (cc BlogsController) UpdateBlogs(c *gin.Context) {
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	blog := models.Blog{}
+	blog, err := cc.blogServices.GetOneBlog(id)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error finding Blog", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to get Blog")
+		responses.HandleError(c, err)
+		return
+	}
+
+	if err := c.ShouldBind(&blog); err != nil {
+		cc.logger.Zap.Error("Error [UpdateBlog] (ShouldBindJson) : ", err)
+		responses.ErrorJSON(c, http.StatusBadRequest, "Failed to update blog")
+		return
+	}
+
+	blog.ID = id
+
+	if err := cc.blogServices.UpdateBlogs(blog); err != nil {
+		cc.logger.Zap.Error("Error updating blog", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to update Blog")
+		responses.HandleError(c, err)
+		return
+	}
+	responses.SuccessJSON(c, http.StatusOK, "Updated")
+}
+
+func (cc BlogsController) DeleteBlog(c *gin.Context) {
+	blogId, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	err := cc.blogServices.DeleteBlog(blogId)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error Deleting Blog", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to Delete Blog")
+		responses.HandleError(c, err)
+		return
+	}
+	responses.SuccessJSON(c, http.StatusOK, "Deleted")
 }
